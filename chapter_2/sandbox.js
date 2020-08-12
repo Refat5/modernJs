f35db29c69a252078b552a86ec8a66e9b140e344
Refat5 <mdrefat382139.com>
2020-08-12 20:28:00 +0600

    first Uplode

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,16 @@
+////alert('HI Refat');
+// console.log(1);
+// console.log(2);
+
+let name='refat';
+name='Refat';
+console.log(name);
+
+const roll=40;
+//this is not work for const
+// roll=489;
+console.log(roll);
+
+var age=643;
+age= 423;
+console.log(age);
