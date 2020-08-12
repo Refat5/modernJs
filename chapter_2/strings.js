0921104d2905cbf7b6191e050232c38366b89534
Refat5 <mdrefat382139.com>
2020-08-12 21:26:05 +0600

    learn about string

---
+++ chapter_2/strings.js
@@ -0,0 +1,29 @@
+//strings
+console.log('HI Refat');
+
+let email='refat342@gmail.com';
+console.log(email);
+
+//string concatenation
+let firstName = 'Abdur';
+let lastName = 'Rahim';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName); 
+
+
+//getting characters 
+console.log(fullName[4]);
+
+//string length
+console.log(firstName.length);
+
+//string methods 
+
+console.log(fullName.toUpperCase());
+let lower = lastName.toLowerCase();
+
+console.log(lower);
+
+const index = email.indexOf('g');
+console.log(index);
