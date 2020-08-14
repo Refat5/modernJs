8c28a8f03c70152171a1819eb5b7cd8acfefe45a
Refat5 <mdrefat382139.com>
2020-08-14 15:59:03 +0600

    learn math operatior

---
+++ chapter_2/number.js
@@ -0,0 +1,34 @@
+let radius = 20;
+const pi = 4.20;
+
+console.log(radius, pi);
+
+//Math Operations for programming + , - , * , / , **, % 
+
+console.log(50/4);
+
+// let answer = radius % 3;
+// console.log(answer);
+
+//order of operation - 
+// let answer = 6 * (9-5)**2;
+// console.log(answer);
+
+let number = 20;
+
+// number = number + 3;
+// number ++;
+//number --;
+// number += 5;
+// number -= 7;
+// number /= 5;
+//number *= 5;
+//console.log(number);
+
+
+//Nan not a number
+
+let result = 'the number is ' + number  +  'number';
+console.log(result);
+
+
