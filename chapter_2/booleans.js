ae641b45606c9d5d4c00d2215a5a66bcd43da229
Refat5 <mdrefat382139.com>
2020-08-15 18:22:13 +0600

    learn boolean (true,false) and comparison

---
+++ chapter_2/booleans.js
@@ -0,0 +1,52 @@
+//Boolean and Comparisons
+
+console.log(true, false, "true" ,"false");
+
+//Methods Can return booleans 
+
+// let email = 'mdrahim21@gmai.com';
+// let name = ['refat','asgu','tabu'];
+
+// // let answer = email.includes(21);
+// let answer = name.includes('djslk');
+
+// console.log(answer);
+
+//Comparison Operatiors
+
+
+//for intiger
+// let number = 40;
+
+// console.log(number == 39);
+// console.log(number == 40);
+// console.log(number != 30);
+// console.log(number < 90);
+// console.log(number > 90);
+// console.log(number <= 40);
+// console.log(number >= 40);
+
+//for string 
+
+let name = 'refat';
+console.log(name == 'sheft');
+console.log(name == 'refat');
+console.log(name != 'sheft');
+console.log(name > 'sheft');
+console.log(name <'sheft');
+console.log(name <= 'sheft');
+console.log(name >= 'sheft');
+console.log(name < 'Refadsdt');
+console.log(name == 'Refat');
+console.log(name >= 'Refat');
+
+
+
+
+
+
+
+
+
+
+
