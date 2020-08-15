af7872d3918d5ba099d1da33e29ec40698429c1f
Refat5 <mdrefat382139.com>
2020-08-15 17:43:53 +0600

    learn array methods

---
+++ chapter_2/array.js
@@ -0,0 +1,22 @@
+let name = ['refat','tanvir','jubair'];
+
+// name[2] = 'komol';
+// console.log(name);
+
+// let ages = [22,32,34,32];
+//  console.log(ages[3]);
+
+// let random = [22,32,34,'refat','ashik','samad'];
+//  console.log(random);
+
+//console.log(name.length);
+
+//Array Methods
+
+// let answer = name.join('-');
+//let answer = name.indexOf('refat');
+//let answer = name.concat(['samad','sagor']);
+let answer = name.push('partho');
+answer = name.pop();
+
+ console.log(answer);
