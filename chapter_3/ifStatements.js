78bd948006f03160b4b93acb6f2db7cfd9f664a9
Refat5 <mdrefat382139.com>
2020-08-17 08:23:33 +0600

    if statement

---
+++ chapter_3/ifStatements.js
@@ -0,0 +1,20 @@
+//If Statements
+
+// const year=2010;
+
+// if(year==2010){
+//     console.log('this year is 2010')
+// }
+
+// const name=['rfat','ami','tumi'];
+
+// if(name.length > 0){
+//     console.log('this name  is refat');
+// }
+
+const password='rfa23t';
+
+
+if(password.length >= 6){
+    console.log('this password  is strong');
+}
