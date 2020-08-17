abe99e07c8bfd8c3efd5bad56affbde045e49cec
Refat5 <mdrefat382139.com>
2020-08-17 07:55:08 +0600

    while loop

---
+++ chapter_3/whileLoops.js
@@ -0,0 +1,16 @@
+//While loops 
+
+// let i = 0;
+
+// while(i < 4){
+//     console.log('the loop is',i);
+//     i++;
+// }
+
+ let i = 0;
+const names = ['refat','tanvir','jubair'];
+
+while(i < names.length){
+    console.log('the name is',names[i]);
+    i++;
+}
