47b4b25e1b76ea8f43860f3635a44be4928945f8
Refat5 <mdrefat382139.com>
2020-08-17 07:39:32 +0600

    Control flow first class in for loop

---
+++ chapter_3/forLoop.js
@@ -0,0 +1,15 @@
+//For Loops 
+
+// for(let i = 0; i < 3; i++){
+//     console.log('the Loop is :', i);
+// }
+
+const names = ['refat','tanvie','jubair'];
+
+for (let i = 0; i< names.length; i++){
+    // console.log(names[i]);
+
+   let html =`<div>${names[i]}</div>`
+   console.log(html);
+}
+
