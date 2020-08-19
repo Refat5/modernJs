c12f43744ba247808761e537504858bf4e5b61b6
Refat5 <mdrefat382139.com>
2020-08-19 16:19:29 +0600

    learn functon

---
+++ chapter_4/function.js
@@ -0,0 +1,16 @@
+//Function 
+collage();
+//function expression 
+//this function you can use after the function
+let name = function(){
+    console.log('my name is refat');
+};
+name();
+name();
+collage();
+//function declaration
+
+function collage(){
+console.log('feni computer Institute');
+}
+collage();
