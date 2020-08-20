e892b2ab7cb21365c58a9a6557a7ffb323f1a291
Refat5 <mdrefat382139@gmail.com>
2020-08-20 22:57:15 +0600

    learn call back function

---
+++ chapter_4/returingValue.js
@@ -0,0 +1,15 @@
+const name = function(number){
+    let result = 3 +  number +30;
+  
+    return result;
+};
+
+const result = name(6);
+console.log(result);
+
+const hi=function(result)
+{
+
+};
+hi(result);
+
