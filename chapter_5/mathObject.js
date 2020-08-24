d46066daeed125b919f6c03d36439bac4489bf81
Refat5 <mdrefat382139@gmail.com>
2020-08-24 18:19:50 +0600

    lear math object and object in Array

---
+++ chapter_5/mathObject.js
@@ -0,0 +1,19 @@
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+
+
+// const number = 9.7;
+
+// console.log(Math.round(number));
+// console.log(Math.floor(number));
+// console.log(Math.trunc(number));
+// console.log(Math.ceil(number));
+
+///random number 
+const ran = Math.random();
+
+// console.log(ran);
+
+console.log(Math.round(ran * 10));
+console.log(typeof Math);
